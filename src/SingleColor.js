700802a8aaa5690178a5f66abe1f9aaf957339f6
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-12-23 11:45:16 +0200

    add single color component

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
-const SingleColor = () => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
     return (
         <div>
             
